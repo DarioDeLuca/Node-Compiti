2ad13207e26b5161bc282e14b48dce71034ffde2
Dario <dario251102@gmail.com>
2021-01-19 18:38:10 +0100

    concluso compito API

--- Api 16.01.21/core/commenti.js
+++ Api 16.01.21/core/commenti.js
@@ -12,12 +12,12 @@ e.getComment = async(id,response) => {
 
   let comment = await c.forEach( (e) => {
     if (e.id==id){
-      response.write( `<h1>Ecco le risposte al post "${e.title}" dell'user avente Id = ${e.userId}:</h1><br>`) 
+      response.write( `<h1>Ecco le risposte al post "${e.title}" pubblicato dall'utente ${e.userId}:</h1><br>`) 
     }  
   })
 
   let nomi = await dati.forEach( (e) => {
-    response.write( `<h3>"${e.email}" ha commentato: </h3> <p>${e.body}</p>`)   
+    response.write( `<h3>"${e.email}" ha commentato: </h3> <p>${e.body}</p><br>`)   
   })
   response.send(comment)
   response.send(nomi)
@@ -36,18 +36,21 @@ e.listaPost = async(response) => {
   let post = await p.forEach( (e) => {
     
     if(x===0){
-      response.write( `<h1> Ecco la lista dei post pubblicati:</h1><br> `)
+      response.write( `<h1> Ecco la lista dei post pubblicati:</h1> `)
+      response.write( `<br><h1> Ecco la lista dei post pubblicati dall'utente ${e.userId} : </h1><br>`)
       x = x+1
     }
 
       if (e.userId==c){
-        response.write( `<h3> L'utente "${e.userId}" ha pubblicato il seguente post ( id = ${e.id} ) dal titolo "${e.title}":</h3><p>${e.body}</p> `)   
+       
+        response.write( `<h3>"${e.title}":</h3><p>${e.body}</p><br> `)   
       }
 
    
       else{
         c=c+1
-        response.write( `<h3> L'utente "${e.userId}" ha pubblicato il seguente post ( id = ${e.id} ) dal titolo ${e.title}:</h3><p>${e.body}</p> `)
+        response.write( `<br><h1> Ecco la lista dei post pubblicati dall'utente ${e.userId} :</h1><br> `)
+        response.write( `<h3>"${e.title}":</h3><p>${e.body}</p><br> `)
     } 
 
   })
